7ef961a43d5964dad39f3411705c95fa8972e14f
Jmart5564 <Jmart5564@gmail.com>
2022-07-13 16:59:43 -0700

    images upload to supabase

--- services/posts-service.js
+++ services/posts-service.js
@@ -16,11 +16,60 @@ export async function getAllPosts() {
     return checkResponse(response);
 }
 
-export async function addPost(post) {
-    const response = await client
+export async function addPost(post, profile) {
+    // If there's an image, hold off on inserting it into database
+    const image = post.image;
+    if (image && image.size) delete post.image;
+
+    // Associate user w/ post
+    post.user_id = profile.id;
+
+    let response = await client
         .from(POST_TABLE)
         .insert(post)
         .single();
 
-    return checkResponse(response);
+    const newPost = checkResponse(response);
+    
+    // If we have an image, upload it and use the post id as a unique
+    // identifier for naming.
+    if (image && image.size) {
+        const imageUrl = await uploadPhoto(image, profile, newPost.id);
+
+        response = await client
+            .from(POST_TABLE)
+            .update({
+                image: imageUrl
+            })
+            .match({
+                id: newPost.id
+            })
+            .single();
+
+        return checkResponse(response);
+    }
+
+    return newPost;
+}
+
+async function uploadPhoto(photo, profile, postId) {
+    const ext = photo.type.split('/')[1];
+    let imageName = `/${profile.id}/${postId}.${ext}`;
+
+    let response = await client.storage
+        .from('images')
+        .upload(imageName, photo, {
+            cacheControl: '3600',
+            upsert: true
+        });
+
+    if (!checkResponse(response)) return null;
+
+    response = client.storage
+        .from('images')
+        .getPublicUrl(imageName);
+
+    if (!checkResponse(response)) return null;
+
+    return response.publicURL;
 }
